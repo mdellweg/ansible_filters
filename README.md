e14360b47115c5f9263c84fdc2c8f88587528de3
Matthias Dellweg <2500@gmx.de>
2020-10-29 12:20:50 +0100

    Add canonical_semver filter

--- README.md
+++ README.md
@@ -17,4 +17,9 @@ An Ansible collection of random filters I missed at some point.
   lineinfile:
     dest: settings.py
     line: "CONSTANT = {{ conf_dict.key2 | mdellweg.filters.repr }}"
+
+- name: Check expected version
+  assert:
+    that:
+      - reported_version | mdellweg.filters.canonical_semver == (expected_version | mdellweg.filters.canonical_semver)
 ```
--- plugins/filter/filters.py
+++ plugins/filter/filters.py
@@ -11,10 +11,18 @@ try:
 except ImportError:
     HAS_JQ = False
 
+try:
+    from packaging.version import parse as parse_version
+
+    HAS_PACKAGING = True
+except ImportError:
+    HAS_PACKAGING = False
+
 from ansible.module_utils import six
 from ansible.module_utils.basic import missing_required_lib
 from ansible.module_utils._text import to_native, to_text
 from ansible.errors import AnsibleError
+
 
 __metaclass__ = type
 
@@ -48,9 +56,17 @@ def repr_filter(value):
     return to_text(repr(map_to_native(value)))
 
 
+def canonical_semver_filter(value):
+    """
+    Represent a semantic version in a cononical form.
+    """
+    return to_text(str(parse_version(to_native(value))))
+
+
 class FilterModule(object):
     def filters(self):
         return {
+            "canonical_semver": canonical_semver_filter,
             "jq": jq_filter,
             "repr": repr_filter,
         }
